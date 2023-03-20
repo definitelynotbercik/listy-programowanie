7263c56737324c2d56233347870c41ec773c07ce
Hubert Zawerbny <zawerbnyhubert@gmail.com>
2023-03-20 14:30:29 +0100

    zad1 - generator hasel

---
+++ lista2/l2zad1.py
@@ -0,0 +1,10 @@
+import string
+import random
+
+def password_generator(length = 8, signs = string.ascii_letters + string.digits + string.punctuation):
+    """ INSERT DOCSTRING """
+    if type(signs) != str:
+        raise TypeError("Sings type have to string")
+    return "".join([random.choice(signs) for i in range(length)])
+
+print(password_generator(150,123))
