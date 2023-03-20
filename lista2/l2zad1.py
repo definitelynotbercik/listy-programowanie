c4af97a7eb66e9b19de997a05b5d7fe30764376d
Hubert Zawerbny <zawerbnyhubert@gmail.com>
2023-03-20 17:18:19 +0100

    poprawa literowki

--- lista2/l2zad1.py
+++ lista2/l2zad1.py
@@ -4,7 +4,7 @@ import random
 def password_generator(length = 8, signs = string.ascii_letters + string.digits + string.punctuation):
     """ INSERT DOCSTRING """
     if type(signs) != str:
-        raise TypeError("Sings type have to string")
+        raise TypeError("Sings type have to be string")
     return "".join([random.choice(signs) for i in range(length)])
 
 print(password_generator(150,123))
