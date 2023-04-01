3d080003080c731bed43fc61673172a71d352972
Hubert Zawerbny <zawerbnyhubert@gmail.com>
2023-04-01 23:25:19 +0200

    zad6_v2

--- lista2/l2zad6.py
+++ lista2/l2zad6.py
@@ -75,30 +75,55 @@ def column_multiply(operation:str):
     result = eval(operation)
     op_list = operation.split("*")
 
+    add_list = []
+    complete_add_list = []
+    for i in range(len(op_list[1])-1, -1, -1):
+        carry = 0
+        for j in range(len(op_list[0])-1, -1, -1):
+            prod = int(op_list[1][i]) * int(op_list[0][j]) + carry
+            carry = prod//10
+            prod %= 10
+            add_list.append(prod)
+        if carry != 0:
+            add_list.append(carry)
+        complete_add_list.append("".join(map(str, add_list[::-1])))
+        add_list = []
+
     len_list = []
     for i in range(len(op_list)):
         len_list.append(len(op_list[i]))
     len_list.append(len(str(result)))
-    
-    max_length = max(len_list) + 3
 
-    str_op_list = []
-    for i in range(len(op_list)-1):
-        str_op_list.append(op_list[i].rjust(max_length))
+    for i in range(len(complete_add_list)):
+        len_list.append(len(complete_add_list[i]))
     
-    str_op_list.append("*" + op_list[-1].rjust(max_length-1))
+    max_length = max(len_list) + len(complete_add_list) + 1
+
+    multi_op_list = []
+    for i in range(len(op_list)-1):
+        multi_op_list.append(op_list[i].rjust(max_length))
+    
+    multi_op_list.append("*" + op_list[-1].rjust(max_length-1))
     str_result = str(result).rjust(max_length)
     line = "-"*max_length
 
+    add_op_list = []
+    for i in range(len(complete_add_list)-1):
+        add_op_list.append(complete_add_list[i].rjust(max_length-i))
+
+    add_op_list.append("+" + complete_add_list[-1].rjust(max_length-len(complete_add_list)))
+
     column = ""
-    for i in range(len(str_op_list)):
-        column += str_op_list[i] + "\n"
+    for i in range(len(multi_op_list)):
+        column += multi_op_list[i] + "\n"
+    column += line + "\n"
+    for i in range(len(add_op_list)):
+        column += add_op_list[i] + "\n"
     column += line + "\n" + str_result
 
     return column
 
 if __name__ == "__main__":
     print(column_add("5+5-300+5-300-800-5+8"))
-
-    print(column_multiply("235*72"))
-
+    print("/////////////////////////")
+    print(column_multiply("100*500"))
