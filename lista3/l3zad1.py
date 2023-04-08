c58f992522642d9e25e8a1d9807790c78355183c
Hubert Zawerbny <zawerbnyhubert@gmail.com>
2023-04-08 20:53:16 +0200

    zad1_v1

---
+++ lista3/l3zad1.py
@@ -0,0 +1,52 @@
+import zipfile
+import os
+import datetime
+import time
+
+def add_folder_and_date(path:str) -> str:
+    """
+    Modify file path adding new folders and current date
+
+    ...
+
+    Input
+    ----------
+    path (str): The file path to modify
+
+    Output
+    ----------
+    new_path (str): The modified file path
+    """
+
+    date = datetime.datetime.today().strftime('%Y-%m-%d')
+    os.mkdir(path+"\\backup")
+    new_path = path + f"\\backup\\copy-{date}.zip"
+    return new_path
+
+
+def backup_copy(input_path:list, output_path:str, extensions:list=[], max_mod_time=10):
+    """INSERT DOCSTRING"""
+    
+    output_path = add_folder_and_date(output_path)
+    current_time = time.time()
+    sec_max_mod_time = max_mod_time*86400
+
+    with zipfile.ZipFile(output_path, 'w') as zip:
+        for file in input_path:
+            main = os.walk(file)
+            for roots, folders, files in main:
+                for file_name in files:
+                    path = os.path.join(roots, file_name)
+                    path_ext = path.split('.')[-1]
+                    mod_date = os.path.getmtime(path)
+                    date_diff = current_time - mod_date
+                    print(path)
+                    print(mod_date)
+                    if (path != output_path and
+                        (path_ext in extensions or extensions == []) and
+                        sec_max_mod_time >= date_diff):
+                        zip.write(path)
+
+
+if __name__ == "__main__":
+    backup_copy(["C:\\Users\\zawer\\Documents\\python1\\extras"], "C:\\Users\\zawer\\Documents\\python1\\extras")
