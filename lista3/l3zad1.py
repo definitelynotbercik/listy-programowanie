a0b559b198d8d9231d3773d36746a88a36b7eb1f
Hubert Zawerbny <zawerbnyhubert@gmail.com>
2023-04-09 12:35:21 +0200

    zad1_v2

--- lista3/l3zad1.py
+++ lista3/l3zad1.py
@@ -19,7 +19,8 @@ def add_folder_and_date(path:str) -> str:
     """
 
     date = datetime.datetime.today().strftime('%Y-%m-%d')
-    os.mkdir(path+"\\backup")
+    if  not os.path.exists(path + "\\backup"):
+        os.mkdir(path + "\\backup")
     new_path = path + f"\\backup\\copy-{date}.zip"
     return new_path
 
@@ -27,6 +28,17 @@ def add_folder_and_date(path:str) -> str:
 def backup_copy(input_path:list, output_path:str, extensions:list=[], max_mod_time=10):
     """INSERT DOCSTRING"""
     
+    if type(extensions) != list or type(extensions) != str:
+        raise TypeError("Given extensions have to be of type list or string")
+    
+    if type(extensions) == list:
+        for item in extensions:
+            if type(item) != str:
+                raise TypeError("Items in extensions list have to be of type string")
+
+    if type(max_mod_time) != int or max_mod_time <= 0:
+        raise TypeError("Given max modification time of files has to be a positive integer")
+
     output_path = add_folder_and_date(output_path)
     current_time = time.time()
     sec_max_mod_time = max_mod_time*86400
@@ -40,13 +52,12 @@ def backup_copy(input_path:list, output_path:str, extensions:list=[], max_mod_ti
                     path_ext = path.split('.')[-1]
                     mod_date = os.path.getmtime(path)
                     date_diff = current_time - mod_date
-                    print(path)
-                    print(mod_date)
                     if (path != output_path and
                         (path_ext in extensions or extensions == []) and
                         sec_max_mod_time >= date_diff):
-                        zip.write(path)
+                        #zip.write(path)
+                        print(f"Saved {path}")
 
 
 if __name__ == "__main__":
-    backup_copy(["C:\\Users\\zawer\\Documents\\python1\\extras"], "C:\\Users\\zawer\\Documents\\python1\\extras")
+    backup_copy(["C:\\Users\\zawer\\Documents\\python1\\extras"], "C:\\Users\\zawer\\Documents\\python1\\extras", "jpg,txt", 30)
