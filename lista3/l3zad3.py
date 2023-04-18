fe20fa10214e7e0f2208dddf48b82be959f37843
Hubert Zawerbny <zawerbnyhubert@gmail.com>
2023-04-18 13:34:56 +0200

    zad3_v2

--- lista3/l3zad3.py
+++ lista3/l3zad3.py
@@ -1,17 +1,30 @@
 import PyPDF2
 import os
+from natsort import natsorted
 
 def merge_pdf(input_path, output_path):
+    """INSERT DOCSTRING"""
+
     merger = PyPDF2.PdfMerger()
     main = os.walk(input_path)
+    merge_list = []
 
     for (roots, folders, files) in main:
         for file_name in files:
-            print(os.path.join(roots, file_name))
-            merger.append(os.path.join(roots, file_name))
+            merge_list.append(os.path.join(roots, file_name))
+
+    merge_list = natsorted(merge_list)
+    print(merge_list)
+
+    for file in merge_list:
+        merger.append(file)
 
     with open(output_path, "wb") as out:
         merger.write(out)
+
+
+def compare_pdfs(pdf1, pdf2):
+    pass
 
 
 if __name__ == "__main__":
