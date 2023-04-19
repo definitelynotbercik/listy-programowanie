9a518e93d915521f504dfbc4b337d7487392f5e9
Hubert Zawerbny <zawerbnyhubert@gmail.com>
2023-04-19 02:16:49 +0200

    zad4_v2

--- lista3/l3zad4.py
+++ lista3/l3zad4.py
@@ -1,11 +1,24 @@
 import qrcode
+import cv2
 
 def generate_QRcode(message, output_path):
+    """INSERT DOCSTRING"""
+
     qr = qrcode.QRCode()
     qr.add_data(message)
     qr.make(fit=True)
-    qr_image = qr.make_image(fill_color="white", back_color="black")
+    qr_image = qr.make_image()
     qr_image.save(output_path)
 
+
+def read_QRcode(qr):
+    """INSERT DOCSTRING"""
+    
+    img = cv2.imread(qr)
+    det = cv2.QRCodeDetector()
+    value, points, straight_qrcode = det.detectAndDecode(img)
+    return value
+
 if __name__ == "__main__":
-    generate_QRcode("abcd", "C:\\Users\\zawer\\Documents\\python1\\extras\\qrcode.png")
+    generate_QRcode("https://media.tenor.com/TRm-7oNu6DwAAAAd/cat.gif", "C:\\Users\\zawer\\Documents\\python1\\extras\\qrcode.png")
+    print(read_QRcode("C:\\Users\\zawer\\Documents\\python1\\extras\\qrcode.png"))
